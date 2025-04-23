269cab03b32bd1219028b1bf5e3ba80bd2e905a8
Vishaljain290502 <vishalj.aurasoft@gmail.com>
2025-04-23 13:35:35 +0530

    push latest code 23-04-25

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,38 +1,33 @@
-import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
-import { MongooseModule } from '@nestjs/mongoose';
 import { HelperModule } from './helper/helper.module';
-
 import { TripModule } from './trip/ride.module';
 import { VehicleModule } from './vehicle/vehicle.module';
 import { BrandModule } from './brand/brand.module';
 
 @Module({
-  imports: [UserModule, TripModule, AuthModule,MongooseModule.forRoot('mongodb+srv://vishaljaurasoft:uWkjdnz06DQ2zDKg@cluster0.lcqe8e7.mongodb.net/rydr?retryWrites=true&w=majority&appName=Cluster0'), HelperModule, TripModule, VehicleModule, BrandModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }), 
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+    UserModule,
+    AuthModule,
+    HelperModule,
+    TripModule,
+    VehicleModule,
+    BrandModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {
-  // configure(consumer: MiddlewareConsumer) {
-  //   consumer
-  //     .apply(AuthMiddleware)
-  //     .exclude(
-  //       { path: 'auth/login', method: RequestMethod.POST },
-  //       { path: 'auth/register', method: RequestMethod.POST },
-  //       { path: 'auth/forgot-password', method: RequestMethod.POST },
-  //       { path: 'auth/reset-password', method: RequestMethod.POST },
-  //       { path: 'auth/verify-phone', method: RequestMethod.POST },
-  //       { path: 'auth/verify-email', method: RequestMethod.POST }
-  //     )
-  //     .forRoutes('*');
-  // }
-}
-
-
-
-
-
-
+export class AppModule {}
